9a00a229dae85a299ea2cbbb424d1436ce176fcd
chokoreetosan <mallepalliabhishek@gmail.com>
2019-04-04 22:34:00 -0700

    added a mute button

--- app/app.js
+++ app/app.js
@@ -24,11 +24,11 @@ var loadLocalStorage = function () {
 	$('tbody').html(htmlString);
 	for(var j = 0; j < keys.length;j++){
 		if(JSON.parse(localStorage[keys[j]]).status === 'Active'){
-		htmlString2 += '<img src="' + JSON.parse(localStorage[keys[j]]).feed + '" style="width:300px;height:300px;' + (JSON.parse(localStorage[keys[j]]).hasOwnProperty('style')? JSON.parse(localStorage[keys[j]]).style: '')+'">';
-		htmlString2 += '<audio src="' + JSON.parse(localStorage[keys[j]]).audio + '" loop autoplay></audio>';
+		htmlString2 += '<img class = \'animalfeeds\' src="' + JSON.parse(localStorage[keys[j]]).feed + '" style="width:300px;height:300px;' + (JSON.parse(localStorage[keys[j]]).hasOwnProperty('style')? JSON.parse(localStorage[keys[j]]).style: '')+'">';
+		htmlString2 += (JSON.parse(localStorage[keys[j]]).hasOwnProperty('audio')? '<audio src="' + JSON.parse(localStorage[keys[j]]).audio + '" loop autoplay' + (JSON.parse(localStorage[keys[j]]).mute ==='yes'? ' muted': '' ) + '></audio>' : '');
 		}
 	}
-	$('#animalfeeds').html(htmlString2);
+	$('#feedcontainer').html(htmlString2);
 };
 
 var updateStatusLabel = function(message) {
@@ -69,78 +69,21 @@ $(document).ready(function () {
 				updateStatusLabel('animal does not exist');
 			}
 			loadLocalStorage();
+		});
+		$('#btn-mute-toggle').on('click', function(e){
+			var name = $('#name').val();
+			if(localStorage.hasOwnProperty(name)){
+				if(JSON.parse(localStorage[name]).hasOwnProperty('audio')){
+						var temp = JSON.parse(localStorage[name]);
+						temp.mute = (temp.mute === 'yes'? 'no': 'yes');
+						localStorage.setItem(name, JSON.stringify(temp));
+				}else{
+				updateStatusLabel('animal has no audio');
+				}
+			}else{
+			updateStatusLabel('animal does not exist');
+			}
+		loadLocalStorage();
 		})
 
-/**	$('#btn-update').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var existingValue = localStorage.getItem(key)
-		var keyExists = existingValue !== null;
-
-		if (value === existingValue) {
-			updateStatusLabel('key not updated - that value already exists silly! xD')
-		} else if (keyExists) {
-			updateEntry(key, value);
-			updateStatusLabel('key updated - ' + key);
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		} else {
-			updateStatusLabel('key doesn\'t exist, please use create button instead! :D');
-		}		
-		
-		loadLocalStorage();		
-	});
-
-	$('#btn-delete').on('click', function(e) {
-		var key = $('#key').val();
-		var value = $('#value').val();
-		var keyExists = localStorage.getItem(key) !== null;
-
-		if (keyExists) {
-			removeEntry(key);
-			updateStatusLabel('key removed - ' + key);
-		} else if (key === '') {
-			updateStatusLabel('invalid input!')
-		} else {
-			updateStatusLabel('key doesn\'t exist, nothing removed. :|');
-		}
-
-		loadLocalStorage();
-	});	
-**/
 });
-/*
-
-
-
-When an input element is given a name, that name becomes a property of the owning form element's HTMLFormElement.elements property. That means if you have an input whose name is set to guest and another whose name is hat-size, the following code can be used:
-
-let form = document.querySelector("form");
-let guestName = form.elements.guest;
-let hatSize = form.elements["hat-size"];
-*/
-
-/*
-PAGE CONTENT STUFF
-*/
-//something to update the table every time localStorage changes
-
-//localStorage stuff
-//https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
-////create new entry
-//localStorage.setItem(key, value)
-var createEntry = function(key, value) {
-	return localStorage.setItem(key, value);
-}
-
-////Update existing entry
-//localStorage.setItem(key, value)
-var updateEntry = function(key, value) {
-	return localStorage.setItem(key, value);
-}
-
-////delete existing entry
-//localStorage.removeItem(key)
-var removeEntry = function(key) {
-	return localStorage.removeItem(key);
-}
